61342398ec7c8c58859c6340723328f35ab6ef88
Yobuqui <yobuqui@gmail.com>
2023-11-12 11:04:41 +0100

    Entrega completada con bonus. No entiendo el error que genera el bonus y he cambiado partes del assert.strictEqual de la funcion 03

--- 03-functions.test.js
+++ 03-functions.test.js
@@ -38,20 +38,19 @@ describe("Funciones en JavaScript", () => {
         let rainProbability = "10%";
 
         // Escribe aquí tu función "weatherReport" (fíjate en la aserción para saber qué debe retornar esta función).
-        function weatherReport(name, userName, city, weather, rainProbability) {
+        function weatherReport(userName, name, city, weather, rainProbability) {
             return `Hola ${name} (${userName}), hoy en ${city} el tiempo es ${weather} y las probabilidades de lluvia son del ${rainProbability}.`;
         }
-
-        assert.strictEqual(weatherReport(name, userName, city, weather, rainProbability)).toEqual("Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.");
-    });
-
+        assert.strictEqual(weatherReport(userName, name, city, weather, rainProbability),"Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.");
+    })
 
 
     // BONUS: Crea un test que te permita testear la función que verás a continuación. Para ello, también te hemos proporcionado las variables que necesitarás (y alguna más que no necesitarás, para confundir :P).
     // Recuerda quitar la puntuación de comentario (/* ... */) para que este código a continuación sea legible, e inclúyelo dentro de tu test.
 
 
-     /*    let userName = "Ariana28";
+    it("bonus", () => {
+        let userName = "Ariana28";
         let userAge = 38;
         let userEmail = "ariana_28@gmail.com";
         let userLocation = "Barcelona"
@@ -59,8 +58,7 @@ describe("Funciones en JavaScript", () => {
         let password = "p4s$w0rDs3guR0";
 
         function userAuth(userName, userEmail, password) {
-            return "Welcome ${userName}, your email is ${userEmail} and your password is ${password}."
-        } */
-
-
+            return `Welcome ${userName}, your email is ${userEmail} and your password is ${password}.`;
+        }
+        assert.strictEqual(userAuth(userName, userEmail, password),"Welcome Ariana28, your email is ariana_28@gmail.com and your password is p4s$w0rDs3guR0.");
     })
